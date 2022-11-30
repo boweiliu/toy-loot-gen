6217d5340fa63121de0993ffcc580e18097f2be9
Bowei Liu <liubowei@gmail.com>
2022-11-30 06:50:12 +0000

    sample

--- fe-toy-loot-gen/src/App.tsx
+++ fe-toy-loot-gen/src/App.tsx
@@ -3,9 +3,22 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       foob
     </div>
+    <div>
+      <button>click me</button>
+    </div>
+    <div>
+      <div>
+        Item:
+      </div>
+      <pre>
+        {`Attack: 5-6 plain bludgeoning damage per action.\nMod: -5L piercing resist, -5L slashing resist`}
+      </pre>
+    </div>
+    </>
   );
 }
 
