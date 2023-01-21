2a4cb4b1437247cc2cf2f622965fb9e20b51b7c3
Bowei Liu <liubowei@gmail.com>
2023-01-20 21:18:16 -0800

    frames

--- fe-toy-loot-gen/src/App.tsx
+++ fe-toy-loot-gen/src/App.tsx
@@ -5,18 +5,21 @@ function App() {
   return (
     <>
     <div className="App">
-      foob
-    </div>
-    <div>
-      <button>click me</button>
-    </div>
-    <div>
-      <div>
-        Item:
+      <div className="title">
+        Toy loot gen game
       </div>
-      <pre>
-        {`Attack: 5-6 plain bludgeoning damage per action.\nMod: -5L piercing resist, -5L slashing resist`}
-      </pre>
+      <div className="body-left-small">
+        <button>click me</button>
+        <div>
+          Item:
+        </div>
+        <pre>
+          {`Attack: 5-6 plain bludgeoning damage per action.\nMod: -5L piercing resist, -5L slashing resist`}
+        </pre>
+      </div>
+      <div className="body-right-big">
+        your robot name here
+      </div>
     </div>
     </>
   );
