71a3518fb326a7d6817c1aed82701b55f9771c4f
Bowei Liu <liubowei@gmail.com>
2023-04-22 00:25:19 -0700

    wip start remake simpler

--- fe-toy-loot-gen/src/App.tsx
+++ fe-toy-loot-gen/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {FC} from 'react'
 import { DndContext, useDraggable, useDroppable, DragOverlay } from '@dnd-kit/core'
 import './App.css'
 
@@ -25,52 +25,28 @@ function App() {
 
   return (
     <>
-      <div className="App">
-        <div className="title">
-          <div className="title-text">Toy loot gen game</div>
+      <div className="appContainer">
+        <div className="appTitle">
+          <div className="titleText">Toy loot gen game</div>
         </div>
-        <div className="body">
-            <div className="body-left-small">
-              Library
-              <div className="card-in-library-list">
-                <DndContext>
-                <Draggable />
-                <div className="card-in-library-big">
-                  sample module here - expanded
-                </div>
-                </DndContext>
-                <div className="card-in-library-big">
-                  sample module here - expanded
-                </div>
-                <div className="card-in-library-big">
-                  sample module here - expanded
-                </div>
-              </div>
-            </div>
-            <div className="body-right-big">
-              <div className="hull-wrapper">
-                <div className="hull-title">your robot name here</div>
-                <div className="hull-body">
-                  <div className="hull-column">
-                    <div className="hull-column-strut"></div>
-                    <div className="hull-column-container">
-                      <div ref={setNodeRef} className="hull-column-item">{isOver ? 'slot 1' : 'empty'}</div>
-                      <div className="hull-column-item">slot 2</div>
-                      <div className="hull-column-item">slot 3</div>
-                      <div className="hull-column-item">slot 4</div>
-                    </div>
-                  </div>
-                  <div className="hull-column"></div>
-                  <div className="hull-column"></div>
-                  <div className="hull-column"></div>
-                </div>
-              </div>
-            </div>
+        <div className="appBody">
+          <Cols>
+            <Col>blah</Col>
+            <Col>blah</Col>
+            <Col>blah</Col>
+          </Cols>
         </div>
-        <div className="debug">debug</div>
       </div>
     </>
   )
 }
 
-export default App
+const Cols: FC<{ children?: React.ReactNode }> = ({ children }) => {
+    return (<div className="colsContainer">{children}</div>)
+}
+
+const Col: FC<{ children?: React.ReactNode }> = ({ children }) => {
+    return (<>{children}</>)
+}
+
+export default App;
